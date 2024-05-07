8496f89148cfa82313b1472d7788b0ad2c2a6ff7
Alin Burghila <108270121+burghila@users.noreply.github.com>
2024-05-07 11:56:52 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Program.cs
+using System;  // Import necessary namespace
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the current date and time to the console
+            Console.WriteLine("The current time is: " + DateTime.Now);
+
+            // Ask the user for a number
+            Console.WriteLine("\nEnter a number to see what the time will be in that many hours: ");
+            float answer = float.Parse(Console.ReadLine());  // Parse the user input to a float
+
+            // Print what time it will be in the amount of hours the user input
+            Console.WriteLine($"In {answer} hour(s), it will be {DateTime.Now.AddHours(answer)}");
+        }
+    }
+}
